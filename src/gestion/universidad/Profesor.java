a11b9ea452fb6be11b717d4c9f8f9c7d29caea53
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-03 17:02:19 -0500

    feat: :zap: Implementando la validacion de duplicado de asignaturas

--- src/gestion/universidad/Profesor.java
+++ src/gestion/universidad/Profesor.java
@@ -1,14 +1,25 @@
 package gestion.universidad;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Representa a un Profesor, que es un tipo de Persona.
  */
 public class Profesor extends Persona {
-
+    private Set<String> asignaturas;
+    
     public Profesor(String nombre, String apellido, FechaNacimiento fechaNacimiento, DniPersona dni) {
         super(nombre, apellido, fechaNacimiento, dni);
+        this.asignaturas = new HashSet<>();
     }
-
+    
+    public boolean agregarAsignatura(String asignatura){
+       return asignaturas.add(asignatura);
+    }
+    
+    
+    
     @Override
     public String getRol() {
         return "Profesor";
