a93be274ed4c4d138903fbb732b66c47ad4dacaa
Juan-Tapias <jmartineztapias15@gmail.com>
2025-09-03 17:30:15 -0500

    feat: :zap: Se pueden poner materia a los profesores

--- src/gestion/universidad/Profesor.java
+++ src/gestion/universidad/Profesor.java
@@ -1,16 +1,38 @@
 package gestion.universidad;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Representa a un Profesor, que es un tipo de Persona.
  */
 public class Profesor extends Persona {
-
+    private Set<String> asignaturas;
+    
     public Profesor(String nombre, String apellido, FechaNacimiento fechaNacimiento, DniPersona dni) {
         super(nombre, apellido, fechaNacimiento, dni);
+        this.asignaturas = new HashSet<>();
     }
 
     @Override
     public String getRol() {
         return "Profesor";
+    }
+    
+    public boolean agregarAsignatura(String asignatura) {
+        return asignaturas.add(asignatura); 
+    }
+
+    public boolean eliminarAsignatura(String asignatura) {
+        return asignaturas.remove(asignatura);
+    }
+    
+     public Set<String> getAsignaturas() {
+        return asignaturas;
+    }
+     
+   @Override
+    public String toString() {
+        return super.toString() + " | Rol: " + getRol() + " | Asignaturas: " + asignaturas;
     }
 }
